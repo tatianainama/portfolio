ae5fd49da37ef32b9faec30b0ed82065fab2085a
Tatiana Inama <tatii.oxo@gmail.com>
2022-01-09 18:05:38 +0100

    Parse markdown content from files

--- lib/files.ts
+++ lib/files.ts
@@ -1,24 +1,25 @@
 import matter from 'gray-matter'
 import path from 'path'
 import fs from 'fs'
-
+import { marked }from 'marked';
 import { AboutData, File, JobData, ProjectData, FrontMatterData } from '@/types/index';
 
 const mkDir = (dir: string[]) => path.join(...dir);
-const aboutDirectory = mkDir([process.cwd(), 'content', 'about']);
-const jobsDirectory = mkDir([process.cwd(),'content', 'jobs']);
-const projectsDirectory = mkDir([process.cwd(),'content', 'jobs']);
-const contentDirectory = mkDir([process.cwd(), 'content']);
+
+const ABOUT_DIR = mkDir([process.cwd(), 'content', 'about']);
+const JOBS_DIR = mkDir([process.cwd(),'content', 'jobs']);
+const PROJECTS_DIR = mkDir([process.cwd(),'content', 'jobs']);
 
 const readFile = (fileName: string, directory: string) => {
   const id = fileName.replace(/\.md$/, '');
   const fullPath = mkDir([directory, fileName]);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
   const { data, content } = matter(fileContents);
+  const markedContent = marked(content);
   return {
     id,
     data,
-    content
+    content: markedContent
   }
 }
 
@@ -33,8 +34,8 @@ const ascendingOrder = (a: string, b: string) => {
   return a < b ? 1 : a > b ? -1 : 0;
 }
 
-export const jobsData = getFilesContent<JobData>(jobsDirectory).sort((fileA, fileB) => ascendingOrder(fileA.data.from, fileB.data.from));
+export const jobsData = getFilesContent<JobData>(JOBS_DIR).sort((fileA, fileB) => ascendingOrder(fileA.data.from, fileB.data.from));
 
-export const projectsData = getFilesContent<ProjectData>(projectsDirectory);
+export const projectsData = getFilesContent<ProjectData>(PROJECTS_DIR);
 
-export const aboutData = readFile('summary.md', aboutDirectory) as File<AboutData>;
+export const aboutData = readFile('summary.md', ABOUT_DIR) as File<AboutData>;
