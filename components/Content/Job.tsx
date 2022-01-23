e6558897bfcbafb17bb8b7164ef63fc05546da54
Tatiana Inama <tatii.oxo@gmail.com>
2022-01-23 14:07:51 +0100

    Add more styles to Job component

--- components/Content/Job.tsx
+++ components/Content/Job.tsx
@@ -4,14 +4,16 @@ import Period from '@/components/Period'
 import styles from './Content.module.css'
 
 const Job: FC<{ content: string; data: JobData }> = ({
-  data: { company, jobTitle, tags, from, to, jobTitleStatus, project },
+  data: { company, website, jobTitle, tags, from, to, jobTitleStatus, project },
   content,
 }) => (
   <div className={styles.job}>
     <header>
       <h2>
         {project && `${project} - `}
-        {company}
+        <a href={website} target="_blank" rel="noreferrer">
+          {company}
+        </a>
         <small>
           {jobTitle}
           {jobTitleStatus && <span> [{jobTitleStatus}]</span>}
@@ -23,6 +25,11 @@ const Job: FC<{ content: string; data: JobData }> = ({
       dangerouslySetInnerHTML={{ __html: content }}
       className={styles.jobContent}
     />
+    <ul className={styles.tags}>
+      {tags.map((tag) => (
+        <li key={tag}>{tag}</li>
+      ))}
+    </ul>
   </div>
 )
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
 module.exports = {
   content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: [
+          'Inter',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI',
+          'Roboto',
+          'Oxygen',
+          'Ubuntu',
+          'Cantarell',
+          'Fira Sans',
+          'Droid Sans',
+          'Helvetica Neue',
+          'sans-serif',
+        ],
+        lekton: ['Lekton', 'Courier', 'monospace'],
+      },
+      fontSize: {
+        xxs: '0.5rem',
+      },
+    },
   },
   plugins: [],
 }
